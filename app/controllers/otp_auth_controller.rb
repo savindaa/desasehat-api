4447cb72067e807db5f87af08ef1d03d010be9c1
Savino Rifky <savinorifky@gmail.com>
2018-03-29 17:24:51 +0700

    OTP Authentication for moblie devices

---
+++ app/controllers/otp_auth_controller.rb
@@ -0,0 +1,51 @@
+class OtpAuthController < ApplicationController
+  require 'open-uri'
+
+  skip_before_action :authorize_request
+
+  # POST user/login
+  def login
+    user = User.find_by(phone: auth_params[:phone])
+    if user
+      #  generating random 4 digit number
+      otp = 4.times.map{rand(10)}.join
+      # store otp result to db
+      user.update(otp: otp)
+      # send_otp(user.phone, otp)
+      # return user name if the record exist
+      render json: { user: { id: user.id, name: user.name } }
+    else
+    # raise Authentication error if credentials are invalid
+    raise(ExceptionHandler::AuthenticationError, Message.user_not_found)
+    end
+  end
+
+  # POST user/verify
+  def verify_otp
+    user = User.find(otp_params[:user_id])
+    if user.otp == otp_params[:otp]
+      auth_token = JsonWebToken.encode(user_id: user.id)
+      render json: { auth_token: auth_token }
+      user.update(otp: nil)
+
+    else
+      raise(ExceptionHandler::AuthenticationError, Message.otp_not_match)
+    end
+  end
+  private
+
+  def auth_params
+    params.permit(:phone)
+  end
+
+  def otp_params
+    params.permit(:user_id, :otp)
+  end
+
+  def send_otp(phone, otp)
+    userkey = ""
+    passkey = "desasehat"
+    link = "https://reguler.zenziva.net/apps/smsapi.php?userkey=#{userkey}&passkey=#{passkey}&nohp=#{phone}&pesan=Silahkan%20masukan%20kode%20berikut%20ke%20aplikasi%20Desasehat%0A%0A#{otp}%0A%0A"
+    @req = URI.parse(link).read
+  end
+end
