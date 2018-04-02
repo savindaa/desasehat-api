ee432b20a826c4d6d7c61e5d9bc5775a155e1ffa
Savino Rifky <savinorifky@gmail.com>
2018-04-02 20:01:43 +0700

    Add Routes for otp_auth controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,20 @@ Rails.application.routes.draw do
 
   root "patients#dashboard"
 
-  resources :articles, path: '/edukasi'
+  resources :articles, path: "/edukasi"
 
   resources :villages, path: "/desa"
 
   resources :patients, path: "/pasien"
 
-  get 'penduduk/update', to: "residents#update_log"
+  get "penduduk/update", to: "residents#update_log"
 
-  get 'penduduk/:id', to: "residents#show"
+  get "penduduk/:id", to: "residents#show"
 
-  post 'admin/login', to: 'authentication#authenticate_admin'
+  post "admin/login", to: "authentication#authenticate_admin"
+
+  post "user/login", to: "otp_auth#login"
+
+  post "user/verify", to: "otp_auth#verify_otp"
 
 end
