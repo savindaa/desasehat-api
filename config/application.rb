f4489802ff82556c0120b8bc4635b6abad4655db
Savino Rifky <savinorifky@gmail.com>
2018-04-03 22:16:35 +0700

    add autoload lib folder conditional instead just autoload_paths

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,8 @@ module DesasehatApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.autoload_paths << Rails.root.join('lib')
+    load_path = Rails.env.production? ? :eager_load_paths : :autoload_paths
+    config.public_send(load_path) << Rails.root.join('lib')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
