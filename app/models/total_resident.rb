626328c94b4c4aa152c58ed159a84d1939990512
Savino Rifky <savinorifky@gmail.com>
2018-04-03 21:21:45 +0700

    use total resident model instead of resident model

--- app/models/total_resident.rb
+++ app/models/total_resident.rb
@@ -1,13 +1,8 @@
-class Resident < ApplicationRecord
+class TotalResident < ApplicationRecord
 
-  # model Association
   belongs_to :village
 
-  # model Validation
-  validates :input_date, :input_type, :total, presence: true
-
-  # enum on type
-  enum input_type: [ :jumlah_penduduk, :penduduk_sakit ]
+  validates :total, presence: true
 
   def village_id
     {
