cd8c9d0630da9e506c49c2426cd9ae1c63972d00
Savino Rifky <savinorifky@gmail.com>
2018-04-05 11:36:23 +0700

    Add dashboard action on patients_controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,13 @@ Rails.application.routes.draw do
   resources :villages, path: "/desa"
   resources :patients, path: "/pasien"
 
+  get "/dashboard", to: "patients#dashboard"
+
   get "desa/:id/penduduk", to: "residents#show"
 
   # login API
   post "admin/login", to: "authentication#authenticate_admin"
   post "user/login", to: "otp_auth#login"
   post "user/verify", to: "otp_auth#verify_otp"
-  
+
 end
