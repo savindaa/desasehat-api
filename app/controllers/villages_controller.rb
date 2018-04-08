88cceab18c66d8e162eec6e12af9d301812430c0
Savino Rifky <savinorifky@gmail.com>
2018-04-09 05:41:03 +0700

    rework village dropdown menu on villages controller

--- app/controllers/villages_controller.rb
+++ app/controllers/villages_controller.rb
@@ -1,41 +1,51 @@
 class VillagesController < ApplicationController
 
-  # find record before action is executed
-  before_action :find_village, only: [ :show ]
-
   skip_before_action :authorize_request
 
-  # return list of all villages
-  # GET /desa
-  def index
-    villages = Village.all.paginate(page: params[:page], per_page: params[:limit] || 10)
-    render json: villages, status: :ok, only: [:id, :kelurahan], methods: [:kecamatan, :kabupaten, :provinsi]
+  def village_dropdown
+    if !params[:prov].blank?
+      province = Province.find(params[:prov])
+      if !params[:kab].blank?
+        regency = province.regencies.find(params[:kab])
+        if !params[:kec].blank?
+          subdistricts = regency.subdistricts.find(params[:kec])
+          render json: subdistricts.villages, only: [:id, :kelurahan], status: :ok
+        else
+          render json: regency.subdistricts, only: [:id, :kecamatan], status: :ok
+        end
+      else
+        render json: province.regencies, only: [:id, :kabupaten], status: :ok
+      end
+    else
+      render json: Province.all, only: [:id, :provinsi], status: :ok
+    end
   end
 
-  # return detail of a village record
-  # GET /desa/:id
-  def show
-    render json: @village, status: :ok, methods: [:kecamatan, :kabupaten, :provinsi]
-  end
+  def village_statistic
+    @date_log = Date.new( date_year, date_month ).end_of_month
+    patients = Patient.where("village_id = ?", params[:id]).where("created_at <= ?", @date_log).where(status: "accepted")
+    unless patients.blank?
+      log = {
+        village_detail: patients.first.village_id,
+        date: "#{I18n.l(@date_log, format: :short)}",
+        total_resident: total_resident,
+        total_sick_residents: patients.size,
+        sick_residents_detail: []
+      }
 
-  # def pick_desa
-  #   if !params[:prov].blank?
-  #     if !params[:kab].blank?
-  #       if !params[:kec].blank?
-  #         subdistricts = Subdistrict.find(params[:kec])
-  #         render json: { provinsi: subdistricts.regency.province.provinsi, kabupaten: subdistricts.regency.kabupaten, kecamatan: subdistricts.kecamatan, list_desa: subdistricts.villages.as_json(only: [:id, :kelurahan]) }, status: :ok
-  #       else
-  #         regency = Regency.find(params[:kab])
-  #         render json: { provinsi: regency.province.provinsi, kabupaten: regency.kabupaten, list_kecamatan: regency.subdistricts.as_json(only: [:id, :kecamatan]) }, status: :ok
-  #       end
-  #     else
-  #       province = Province.find(params[:prov])
-  #       render json: { provinsi: province.provinsi, list_kabupaten: province.regencies.as_json(only: [:id, :kabupaten]) }, status: :ok
-  #     end
-  #   else
-  #     render json: Province.all, only: [:id, :provinsi], status: :ok
-  #   end
-  # end
+      patients.select(:disease_type).distinct.map do |data|
+        current_log = {
+          disease_type: data.disease_type,
+          total: patients.where(disease_type: data.disease_type).size
+        }
+        log[:sick_residents_detail].push(current_log)
+      end
+
+      render json: log, status: :ok
+    else
+      raise ActiveRecord::RecordNotFound, "Data tidak ditemukan"
+    end
+  end
 
   private
 
@@ -47,6 +57,35 @@ class VillagesController < ApplicationController
   # find village based on :id parameter
   def find_village
     @village = Village.find(params[:id])
+  end
+
+  def date_year
+    unless params[:tahun].blank?
+      if params[:tahun].to_i >= 2000
+        params[:tahun].to_i
+      else
+        raise ExceptionHandler::InvalidDate, "Parameter tahun harus lebih dari tahun 2000"
+      end
+    else
+      Date.today.year
+    end
+  end
+
+  def date_month
+    unless params[:bulan].blank?
+      if params[:bulan].to_i > 0 && params[:bulan].to_i < 13
+        params[:bulan].to_i
+      else
+        raise ExceptionHandler::InvalidDate, "Parameter bulan salah"
+      end
+    else
+      Date.today.month
+    end
+  end
+
+  def total_resident
+    people_count = TotalResident.where("village_id = ?", params[:id]).where("created_at <= ?", @date_log).last
+    people_count.blank? ? 0 : people_count.total
   end
 
 end
