105503076d7fb01594897d6e558e573b777ce66f
Savino Rifky <savinorifky@gmail.com>
2018-04-09 05:42:26 +0700

    rework json parse keys on patients list

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,19 +2,17 @@ class PatientsController < ApplicationController
 
   skip_before_action :authorize_request
 
-  def index
-    patients = Patient.all.paginate(page: params[:page], per_page: params[:limit] || 10)
-    render json: patients.as_json(only: [:id, :disease_type, :name, :age, :village_id, :status]), status: :ok
-  end
-
-  def show
-    @patient = Patient.find(params[:id])
-    render json: @patient, status: :ok
-  end
-
-  def dashboard
+  def patients_list
     patients = Patient.where(status: "accepted").paginate(page: params[:page], per_page: params[:limit] || 10)
-    render json: patients.as_json(only: [:id, :disease_type, :name, :age, :village_id]), status: :ok
+    render json: patients, only: [:id, :name, :age, :village_id ], methods: [ :disease, :picture, :validated_by ], status: :ok
+  end
+
+  def patient_detail
+    @patient = Patient.find(params[:id])
+    render json: @patient,
+      status: :ok,
+      methods: [ :disease, :pictures, :inputted_by, :validated_by ],
+      except: [ :disease_type_id, :validated_by_id, :inputted_by_id, :updated_at ]
   end
 
 end
